5ffcef43dbe4019ec51418fd28973099c080bc9d
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-08-25 16:56:35 +0300

    Added better logging

--- src/analyze_video.js
+++ src/analyze_video.js
@@ -1,13 +1,16 @@
 const { DownloaderHelper } = require("node-downloader-helper");
 const { save_dest } = require("../config.json");
 const { randomBytes } = require("crypto");
+const Logger = require("./utils/logger");
+
+const logger = new Logger();
 
 function analyze_video(url) {
     const rng_filename = `${randomBytes(8).toString("hex")}.mp4`;
     // find out some way to do this without a random nodejs module
     const dl = new DownloaderHelper(url, save_dest, { fileName: rng_filename });
 
-    // dl.on("end", () => console.log("download complete < debug"));
+    dl.on("end", () => logger.info(`Download complete -> ${logger.colors.blue}${rng_filename}`));
     dl.start();
 }
 
