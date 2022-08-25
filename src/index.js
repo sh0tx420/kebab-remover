b64819bd83b7ca6159302df3f10e7b574b2dab20
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-08-25 16:56:21 +0300

    Added detection of videos sent from an URL

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const config = require("../config.json");
 const analyze_video = require("./analyze_video");
+const Logger = require("./utils/logger");
+
+// wtf why did i write such a bad implementation
+const logger = new Logger();
 
 const client = new Client({
     intents: [
@@ -11,15 +15,29 @@ const client = new Client({
     ]
 });
 
-client.once("ready", () => {
-    console.log("bot process ready");
+client.once("ready", bot => {
+    for (const i of ["Bot login successful.", `Username: ${bot.user.tag}`, `User ID: ${bot.user.id}`]) {
+        logger.info(i);
+    }
 });
 
+/* Detect mp4 files sent from files */
 client.on("messageCreate", message => {
     const attachment = message.attachments.first();
 
     if (attachment != undefined) {
         if (attachment.url.includes(".mp4")) analyze_video(attachment.url);
+    }
+});
+
+/* Detect videos sent with an URL
+*  Using messageUpdate because that gets called when discord shows an embed
+*  NOTE: this might not work 100% of the time
+*/
+client.on("messageUpdate", (_, message) => {
+    const regex = /https?.*?\.mp4/;
+    if (message.content.match(regex)) {
+        analyze_video(message.content);
     }
 });
 
