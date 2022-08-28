93a32035a725c9095d8f5652b447944e9ffb1c0f
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-08-28 19:31:51 +0300

    Extract the last frame as a png from the downloaded video

--- src/analyze_video.js
+++ src/analyze_video.js
@@ -1,13 +1,14 @@
 const { DownloaderHelper } = require("node-downloader-helper");
 const { save_dest, size_limit } = require("../config.json");
 const { randomBytes } = require("crypto");
+const { resolve } = require("node:path");
 const Logger = require("./utils/logger");
+const ffmpeg = require("fluent-ffmpeg");
 
 const logger = new Logger();
 
 function analyze_video(url) {
     const rng_filename = `${randomBytes(8).toString("hex")}.mp4`;
-    // find out some way to do this without a random nodejs module
     const dl = new DownloaderHelper(url, save_dest, { fileName: rng_filename });
 
     dl.on("download", file => {
@@ -17,7 +18,16 @@ function analyze_video(url) {
             // apparently not possible to delete the file??? (with unlink and resolve)
         }
     });
-    dl.on("end", () => logger.info(`Download complete -> ${logger.colors.blue}${rng_filename}`));
+    dl.on("end", () => {
+        logger.info(`Download complete -> ${logger.colors.blue}${rng_filename}${logger.colors.reset}`);
+
+        // extract last frame of video to check for tiktok
+        ffmpeg(resolve(save_dest, rng_filename)).screenshot({
+            folder: save_dest,
+            timestamps: ["99%"],
+            filename: `${rng_filename}.jpg`,
+        });
+    });
     dl.start();
 }
 
