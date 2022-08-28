e223699995d97334fb8af3517218c031463286ca
sh0tx420 <sh0tx.cosmoline@gmail.com>
2022-08-28 17:59:30 +0300

    Implemented size limit

--- src/analyze_video.js
+++ src/analyze_video.js
@@ -1,5 +1,5 @@
 const { DownloaderHelper } = require("node-downloader-helper");
-const { save_dest } = require("../config.json");
+const { save_dest, size_limit } = require("../config.json");
 const { randomBytes } = require("crypto");
 const Logger = require("./utils/logger");
 
@@ -10,6 +10,13 @@ function analyze_video(url) {
     // find out some way to do this without a random nodejs module
     const dl = new DownloaderHelper(url, save_dest, { fileName: rng_filename });
 
+    dl.on("download", file => {
+        const rounded_size = Math.round(file.totalSize / Math.pow(1024,2)).toFixed(0);
+        if (rounded_size >= size_limit) {
+            dl.pause();
+            // apparently not possible to delete the file??? (with unlink and resolve)
+        }
+    });
     dl.on("end", () => logger.info(`Download complete -> ${logger.colors.blue}${rng_filename}`));
     dl.start();
 }
